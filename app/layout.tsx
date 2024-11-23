a971315556c6463ce3a4514d795972100244c470
kiranyarra3 <kiranyarra3@gmail.com>
2024-11-23 11:46:47 +0530

    Meta data added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import '@/app/ui/global.css'
 import '@/app/ui/fonts'
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Fruit Market Management System",
+  description: "A comprehensive Fruit Market Management System for tracking imports, exports, transactions, and billing. Features include dashboards, daybooks, account ledgers, and automated calculations.",
+  authors:{name:"Yarra Kiran Kumar"},
+  keywords:["fruit market software","market management system","transaction tracking", "import export tracking", "dashboard for market"]
+};
 
 export default function RootLayout({
   children,
